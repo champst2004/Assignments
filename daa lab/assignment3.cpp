08e6f1894372d9ecf32a0c54063ba3cd5072a31e
Shivank Tripathi <st.is.champion@gmail.com>
2025-01-29 17:03:38 +0530

    ass3 done

--- daa lab/assignment3.cpp
+++ daa lab/assignment3.cpp
@@ -15,11 +15,19 @@ class graph{
             }
         }
 
-        void accept(){
+        void create(){
+            char ch;
             for(int i = 0; i < cities; i++){
                 for(int j = 0; j < cities; j++){
-                    cout << "Cost [" << i << "][" << j << "]: ";
-                    cin >> cost[i][j]; 
+                    if(i != j and cost[i][j] == 999){
+                        cout << "Is there connection between " << i << " and " << j << "? (y/n): ";
+                        cin >> ch;
+                        if (ch == 'y'){
+                            cout << "Enter cost: ";
+                            cin >> cost[i][j];
+                            cost[j][i] = cost[i][j];
+                        } 
+                    }
                 }
             }
         }
@@ -34,8 +42,41 @@ class graph{
             }
         }
 
-        void primsAlgorithm(){
+        int primsAlgorithm(int start){
+            int nearest[cities];
+            int n = cities;
+            int t[cities][3];
+            int r = 0;
+            int minCost = 0;
+            int min;
+            int index;
+            nearest[start] = -1;
 
+            for(int i = 0; i <= n; i++){
+                if(i != start) nearest[i] = start;
+            }
+
+            for(int i = 0; i < n - 1; i++){
+                min = 999;
+                for(int j = 0; j < n; j++){
+                    if(nearest[j] != -1 and cost[j][nearest[j]] < min){
+                        index = j;
+                        min = cost[j][nearest[j]];
+                    }
+                }
+
+                t[r][0] = nearest[index];
+                t[r][1] = index;
+                t[r][2] = min;
+                r++;
+                minCost += cost[index][nearest[index]];
+                nearest[index] = -1;
+
+                for(int j = 0; j < n; j++){
+                    if(nearest[j] != -1 and (cost[j][nearest[j]] > cost[j][index])) nearest[j] = index;
+                }
+            }
+            return minCost;
         }
 };
 
@@ -48,13 +89,13 @@ int main(){
         cin >> ch;
         switch(ch){
             case 1:
-                g.accept();
+                g.create();
                 break;
             case 2:
                 g.display();
                 break;
             case 3:
-                g.primsAlgorithm();
+                cout << "The cost is: " << g.primsAlgorithm(0) << endl;
                 break;
             case 4:
                 cout << "Thank You!";
