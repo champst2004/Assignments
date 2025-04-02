d6a63f668d96855348f54230d85138fccd5411c3
Shivank Tripathi <st.is.champion@gmail.com>
2025-04-02 08:45:00 +0530

    ass 6 updated

--- daa lab/assignment6.cpp
+++ daa lab/assignment6.cpp
@@ -6,22 +6,28 @@ int knapsack(int W, int weights[], int profits[], int n) {
 
     for (int i = 0; i <= n; i++) {
         for (int j = 0; j <= W; j++) {
-            if (i == 0 || j == 0)
-                dp[i][j] = 0;
-            else if (weights[i - 1] <= j)
-                dp[i][j] = max(profits[i - 1] + dp[i - 1][j - weights[i - 1]], dp[i - 1][j]);
-            else
-                dp[i][j] = dp[i - 1][j];
+            if (i == 0 || j == 0) dp[i][j] = 0;
+            else if (weights[i - 1] <= j) dp[i][j] = max(profits[i - 1] + dp[i - 1][j - weights[i - 1]], dp[i - 1][j]);
+            else dp[i][j] = dp[i - 1][j];
         }
     }
     return dp[n][W];
 }
 
 int main() {
-    int n = 4;
-    int profits[] = {1, 2, 5, 6};
-    int weights[] = {2, 3, 4, 5};
-    int W = 8;
+    int n, W;
+    cout << "Enter no. of objects: ";
+    cin >> n;
+    cout << "Enter knapsack capacity: ";
+    cin >> W;
+    int profits[n], weights[n];
+
+    for(int i = 0; i < n; i++){
+        cout << "Enter weight of object " << i + 1 << ": ";
+        cin >> weights[i]; 
+        cout << "Enter profit of object " << i + 1 << ": ";
+        cin >> profits[i]; 
+    }
 
     cout << "Maximum profit: " << knapsack(W, weights, profits, n) << endl;
     return 0;
