54bfcf9cf6c54d95007858771e6e16641b09ed1f
Shivank Tripathi <st.is.champion@gmail.com>
2025-04-09 17:55:41 +0530

    completed ass5

--- daa lab/assignment5.cpp
+++ daa lab/assignment5.cpp
@@ -33,31 +33,137 @@ class student{
         }
 
         void createWithoutReplacement(){
-
+            char choice;
+            int loc;
+            hashing temp;
+            fstream f;
+            f.open("student.txt", ios::binary | ios::out);
+            do{
+                cout << "Enter details\n";
+                cout << "Roll No.: ";
+                cin >> rec.rollno;
+                cout << "Name: ";
+                cin >> rec.name;
+                cout << "Marks: ";
+                cin >> rec.marks;
+                loc = rec.rollno % 10;
+                if(h[loc].rollno == -1){
+                    h[loc].rollno = rec.rollno;
+                    h[loc].pos = relt;
+                    f.write((char*)&rec, s);
+                    relt++;
+                }
+                else{
+                    for(int i = 1; i < 10; i++){
+                        loc = (loc + 1) % 10;
+                        if(h[loc].rollno == -1){
+                            h[loc].rollno = rec.rollno;
+                            h[loc].pos = relt;
+                            f.write((char*)&rec, s);
+                            relt++;
+                            break;
+                        }
+                    }
+                }
+                cout << "Do you want to continue? ";
+                cin >> choice;
+            } while(choice == 'Y' || choice == 'y');
+            f.close();
         }
 
         void createWithReplacement(){
-
-        }
-
-        void modify(){
-
-        }
+            char choice;
+            int nloc, loc;
+            hashing temp;
+            fstream f;
+            f.open("student.txt", ios::binary | ios::out);
+            do{
+                cout << "Enter details\n";
+                cout << "Roll No.: ";
+                cin >> rec.rollno;
+                cout << "Name: ";
+                cin >> rec.name;
+                cout << "Marks: ";
+                cin >> rec.marks;
+                loc = rec.rollno % 10;
+                if(h[loc].rollno == -1){
+                    h[loc].rollno = rec.rollno;
+                    h[loc].pos = relt;
+                    f.write((char*)&rec, s);
+                    relt++;
+                }
+                else{
+                    temp.rollno = rec.rollno;
+                    temp.pos = relt;
+                    f.write((char*)&rec,s);
+                    relt++;
+                    if(loc != (h[loc].rollno % 10)){
+                        temp.rollno = h[loc].rollno;
+                        temp.pos = h[loc].pos;
+                        h[loc].rollno = rec.rollno;
+                        h[loc].pos = relt - 1;
+                    }
+                    nloc = (loc + 1) % 10;
+                    while(nloc != loc){
+                        if(h[nloc].rollno == -1){
+                            h[nloc].rollno = temp.rollno;
+                            h[nloc].pos = temp.pos;
+                            break;
+                        }
+                        nloc = (nloc + 1) % 10;
+                    }
+                }
+                cout << "Do you want to continue? ";
+                cin >> choice;
+            } while(choice == 'Y' || choice == 'y');
+            f.close();
+        } 
 
         void retrive(){
-
-        }
-
-        void del(){
-
+            fstream f;
+            f.open("student.txt", ios::binary | ios::in);
+            cout << "Enter the rollno you want to retieve: ";
+            cin >> rec.rollno;
+            int loc = rec.rollno % 10;
+            int i;
+            for(int i = 1; i < 10; i++){
+                if(h[loc].rollno == rec.rollno){
+                    int p = h[loc].pos * s;
+                    f.seekg(p, ios::beg);
+                    f.read((char*)&rec, s);
+                    display();
+                    break;
+                }
+                else{
+                    loc = (loc + 1) % 10;
+                }
+            }
+            if(i > 10) cout << "Record does not exist\n";
+            f.close();
         }
 
         void display(){
-
+            cout << rec.rollno << "\t\t\t" << rec.name << "\t\t" << rec.marks << endl;
         }
 
         void displayAll(){
-
+            cout << "Hash table\n";
+            cout << "Roll No.\tPosition\n";
+            for(int i = 0; i < 10; i++){
+                cout << h[i].rollno << "\t\t" << h[i].pos << endl;
+            }
+            fstream f;
+            f.open("student.txt", ios::binary | ios::in);
+            cout << "Roll No.\t\tName\t\tMarks\n";
+            for(int i = 0; i < 10; i++){
+                if(h[i].rollno != -1){
+                    int p = h[i].pos * s;
+                    f.seekg(p, ios::beg);
+                    f.read((char*)&rec, s);
+                    display();
+                }
+            }
+            f.close();
         }
 };
 
@@ -66,7 +172,7 @@ int main(){
     student s;
     while(1){
         cout << "1. Create by chaining without replacement\n2. Create by chaining with replacement\n";
-        cout << "3. Retrive\n4. Modify\n5. Delete\n6. Display\n7. Exit\n";
+        cout << "3. Retrive\n4. Display\n5. Exit\n";
         cout << "Enter choice: ";
         cin >> ch;
         switch(ch){
@@ -80,15 +186,9 @@ int main(){
                 s.retrive();
                 break;
             case 4:
-                s.modify();
-                break;
-            case 5:
-                s.del();
-                break;
-            case 6:
                 s.displayAll();
                 break;
-            case 7:
+            case 5:
                 cout << "Thank You!";
                 return 0;
         }
