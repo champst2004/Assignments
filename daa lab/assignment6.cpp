fd6d398c833b481ecc8ed92bac4206451c3fe879
Shivank Tripathi <st.is.champion@gmail.com>
2025-04-25 21:16:57 +0530

    added classes in ass7,8

--- daa lab/assignment6.cpp
+++ daa lab/assignment6.cpp
@@ -1,34 +1,38 @@
 #include <iostream>
 using namespace std;
 
-int knapsack(int W, int weights[], int profits[], int n) {
-    int dp[n + 1][W + 1];
+class knapSack{
+    int n, w, profits[100], weights[100];
+    public:
+        void accept(){
+            cout << "Enter no. of objects: ";
+            cin >> n;
+            cout << "Enter knapsack capacity: ";
+            cin >> w;
 
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= W; j++) {
-            if (i == 0 || j == 0) dp[i][j] = 0;
-            else if (weights[i - 1] <= j) dp[i][j] = max(profits[i - 1] + dp[i - 1][j - weights[i - 1]], dp[i - 1][j]);
-            else dp[i][j] = dp[i - 1][j];
+            for(int i = 0; i < n; i++){
+                cout << "Enter weight of object " << i + 1 << ": ";
+                cin >> weights[i];
+                cout << "Enter profit of object " << i + 1 << ": ";
+                cin >> profits[i];
+            }
         }
-    }
-    return dp[n][W];
-}
+        int solve(){
+            int dp[n + 1][w + 1];
+            for(int i = 0; i < n + 1; i++){
+                for(int j = 0; j < w + 1; j++){
+                    if(i == 0 || j == 0) dp[i][j] = 0;
+                    else if(weights[i - 1] <= j) dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weights[i - 1]] + profits[i - 1]);
+                    else dp[i][j] = dp[i - 1][j];
+                }
+            }
+            return dp[n][w];
+        }
+};
 
-int main() {
-    int n, W;
-    cout << "Enter no. of objects: ";
-    cin >> n;
-    cout << "Enter knapsack capacity: ";
-    cin >> W;
-    int profits[n], weights[n];
-
-    for(int i = 0; i < n; i++){
-        cout << "Enter weight of object " << i + 1 << ": ";
-        cin >> weights[i]; 
-        cout << "Enter profit of object " << i + 1 << ": ";
-        cin >> profits[i]; 
-    }
-
-    cout << "Maximum profit: " << knapsack(W, weights, profits, n) << endl;
+int main(){
+    knapSack ks;
+    ks.accept();
+    cout << "Maximum profit: " << ks.solve() << endl;
     return 0;
 }
--- daa lab/assignment7.cpp
+++ daa lab/assignment7.cpp
@@ -1,34 +1,37 @@
 #include <iostream>
 using namespace std;
 
-bool place(int k, int i, int x[]) {
-    for (int j = 1; j < k; j++) {
-        if (x[j] == i || abs(x[j] - i) == abs(j - k))
-            return false;
-    }
-    return true;
-}
+class nQueen {
+public:
+    bool place(int k, int i, int x[]) {
+        for(int j = 1; j < k; j++){
+            if(x[j] == i || abs(x[j] - i) == abs(j - k)) return false;
+        }
+        return true;
+    }   
 
-void nQueen(int k, int n, int x[]) {
-    for (int i = 1; i <= n; i++) {
-        if (place(k, i, x)) {
-            x[k] = i;
-            if (k == n) {
-                for (int j = 1; j <= n; j++)
-                    cout << x[j] << " ";
-                cout << endl;
-            } else {
-                nQueen(k + 1, n, x);
+    void nqueen(int k, int n, int x[]) {
+        for(int i = 1; i <= n; i++){
+            if(place(k, i, x)){
+                x[k] = i;
+                if(k == n){
+                    for(int j = 1; j <= n; j++){
+                        cout << x[j] << " ";
+                    }
+                    cout << endl;
+                }
+                else nqueen(k+1, n, x);
             }
         }
     }
-}
+};
 
 int main() {
+    nQueen a;
     int n;
     cout << "Enter n: ";
     cin >> n;
-    int arr[n] = {0};
-    nQueen(1, n, arr);
+    int arr[11] = {0};
+    a.nqueen(1, n , arr);
     return 0;
 }
